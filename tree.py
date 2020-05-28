d14a98fc0b678791f7e80e860ab0c5bdb96e257e
Michael <37400755+micha31r@users.noreply.github.com>
2020-05-28 18:32:49 +1200

    Update tree.py

--- tree.py
+++ tree.py
@@ -1,29 +1,66 @@
+# Basic binary tree
+
+import random
+
 class Node:
 	def __init__(self, value=0):
 		self.left = None
 		self.right = None
 		self.value = value
 
-	def compare(self,value):
+	def add_node(self,value):
 		if value > self.value:
 			if self.left:
-				self.left.compare(value)
+				self.left.add_node(value)
 			else:
 				self.left = Node(value)
-		if value < self.value:
+		elif value < self.value:
 			if self.right:
-				self.right.compare(value)
+				self.right.add_node(value)
 			else:
 				self.right = Node(value)
 
-class Tree:
-	def __init__(self,value=0):
-		self.root = Node(value) # Starting node
+	def find(self,value):
+		print(self.value,value)
+		if value < self.value:
+			if self.right:
+				return self.right.find(value)
+		elif value > self.value:
+			if self.left:
+				return self.left.find(value)
+		else:
+			return True
+		return False
 
-	def add_node(self,value):
-		self.root.compare(value)
+	def cycle(self):
+		if self.right:
+			self.right.cycle()
+		print(self.value)
+		if self.left:
+			self.left.cycle()
+
+class Tree:
+	def __init__(self):
+		self.root = None # Starting node
+
+	def add_value(self,value):
+		if not self.root:
+			self.root = Node(value)
+		else:
+			self.root.add_node(value)
+
+	def exsist(self,value):
+		return self.root.find(value)
+
+	def sort(self):
+		self.root.cycle()
 
 tree = Tree()
-tree.add_node(20)
-print(tree.root)
+
+# Add random values to the binary tree
+for i in range(random.randint(10,50)):
+	tree.add_value(random.randint(0,500))
+
+# Sort numbers from smallest to biggest
+tree.sort()
 
